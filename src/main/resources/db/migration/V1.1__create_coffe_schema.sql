bbc52a4ef4f1911173cfafbf879f82320e4324d4
ddu0422 <ddu0422@naver.com>
2021-05-12 00:04:38 +0900

    feat: coffee schema 정의

---
+++ src/main/resources/db/migration/V1.1__create_coffe_schema.sql
@@ -0,0 +1,18 @@
+create table coffees
+(
+    id          bigint auto_increment primary key,
+    name        varchar(100),
+    name_en     varchar(100),
+    description text,
+    price       int
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+create table status
+(
+    id        bigint auto_increment primary key,
+    coffee_id bigint,
+    ice       boolean,
+    hot       boolean
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
