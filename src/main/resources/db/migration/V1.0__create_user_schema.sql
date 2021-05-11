8585cd9f7f7df3ef8fdeab23d5982b679e575403
ddu0422 <ddu0422@naver.com>
2021-05-12 00:04:15 +0900

    chore: sql convention 상태로 변경

--- src/main/resources/db/migration/V1.0__create_user_schema.sql
+++ src/main/resources/db/migration/V1.0__create_user_schema.sql
@@ -1,5 +1,6 @@
-create table users(
-    id bigint auto_increment,
-    email varchar(50),
+create table users
+(
+    id       bigint auto_increment primary key,
+    email    varchar(50),
     password varchar(50)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
