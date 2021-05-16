e79bc8775430db08fc692c6167b0f4f70901e783
ddu0422 <ddu0422@naver.com>
2021-05-16 22:22:58 +0900

    chore: 불필오한 패키지 제거

--- src/main/java/per/project/starbucks/domain/BaseEntity.java
+++ src/main/java/per/project/starbucks/domain/BaseEntity.java
@@ -2,13 +2,10 @@ package per.project.starbucks.domain;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 public class BaseEntity {
-
 
     @Column(name = "createdAt")
     protected LocalDateTime createdAt = LocalDateTime.now();
