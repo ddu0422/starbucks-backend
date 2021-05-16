1c9732586b0becdd44f72ba6514fcc02028dbd86
ddu0422 <ddu0422@naver.com>
2021-05-17 01:17:04 +0900

    chore: jpql query convention 수정

--- src/main/java/per/project/starbucks/domain/UserRepository.java
+++ src/main/java/per/project/starbucks/domain/UserRepository.java
@@ -7,10 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query(
-            "select user.id, user.email " +
-                    "from User user " +
-                    "where user.email = :email and user.password = :password"
-    )
+    @Query("select user.id, user.email " +
+            "from User user " +
+            "where user.email = :email and user.password = :password")
     Optional<User> find(String email, String password);
 }
